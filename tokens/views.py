9c8d5f404fd2a12297b539033b1517d453d1ef30
NLesnov <n.lesnov98@gmail.com>
2022-03-10 20:15:08 +0300

    token pagination draft

--- tokens/views.py
+++ tokens/views.py
@@ -1,8 +1,14 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
 
 from .models import Token
+
+
+class ListTokenResultsPagination(PageNumberPagination):
+    page_size = 200
+    max_page_size = 500
 
 
 class CreateTokenView(APIView):
@@ -11,6 +17,7 @@ class CreateTokenView(APIView):
 
 class ListTokenView(GenericAPIView, ListModelMixin):
     queryset = Token.objects.all()
+    pagination_class = ListTokenResultsPagination
     serializer_class = ...
 
 
