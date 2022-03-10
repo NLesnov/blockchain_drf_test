325e3a91997e4d191ce3d8ce4fa91a0fd3eb2058
NLesnov <n.lesnov98@gmail.com>
2022-03-10 23:20:42 +0300

    added "real" token creation method

--- tokens/views.py
+++ tokens/views.py
@@ -1,4 +1,8 @@
+from web3 import Web3
+
 from django.utils.crypto import get_random_string
+from django.conf import settings
+
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.pagination import PageNumberPagination
@@ -6,6 +10,21 @@ from rest_framework.views import APIView
 
 from .models import Token
 from .serializers import TokenSerializer
+
+
+w3 = Web3(Web3.HTTPProvider(settings.BLOCKCHAIN_PROVIDER))
+
+if settings.USE_GETH_POA:
+    from web3.middleware import geth_poa_middleware
+    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+contract_abi = settings.CONTRACT_ABI
+contract_address = settings.CONTRACT_ADDRESS
+
+contract = w3.eth.contract(
+    address=contract_address,
+    abi=contract_abi,
+)
 
 
 class ListTokenResultsPagination(PageNumberPagination):
@@ -22,10 +41,34 @@ class CreateTokenView(GenericAPIView, CreateModelMixin):
         serializer.save(
             unique_hash=unique_hash
         )
-        self.create_smart_contract(serializer.data)
-        
-    def create_smart_contract(self, token_data):
-        ...
+        transaction_hash = self.create_smart_contract(
+            owner=serializer.data['owner'],
+            unique_hash=serializer.data['unique_hash'],
+            media_url=serializer.data['media_url'],
+        )
+        serializer.save(
+            tx_hash=transaction_hash
+        )
+
+    def create_smart_contract(self, owner, unique_hash, media_url):
+        nonce = w3.eth.get_transaction_count(
+            settings.PUBLIC_VALLET_ADDRESS
+        )
+        transaction = contract.functions.mint(
+            owner=owner,
+            uniqueHash=unique_hash,
+            mediaURL=media_url,
+        ).buildTransaction(
+            {
+                'nonce': nonce
+            }
+        )
+        signed_transaction = w3.eth.account.sign_transaction(
+            transaction,
+            private_key=settings.PRIVATE_KEY
+        )
+        transaction_hash = w3.eth.send_raw_transaction(signed_transaction)
+        return transaction_hash
 
 
 class ListTokenView(GenericAPIView, ListModelMixin):
