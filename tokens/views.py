b266351ead0b3ce5888e8e0ecc11d8d750427476
NLesnov <n.lesnov98@gmail.com>
2022-03-10 20:14:39 +0300

    token views draft

--- tokens/views.py
+++ tokens/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Token
+
+
+class CreateTokenView(APIView):
+    ...
+
+
+class ListTokenView(GenericAPIView, ListModelMixin):
+    queryset = Token.objects.all()
+    serializer_class = ...
+
+
+class GetTokenTotalSupplyView(APIView):
+    ...
