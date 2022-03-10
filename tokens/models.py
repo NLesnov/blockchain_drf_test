d92d288e98f7264c7b2f377a5aac586469adcc7a
NLesnov <n.lesnov98@gmail.com>
2022-03-10 20:13:34 +0300

    added Token model

--- tokens/models.py
+++ tokens/models.py
@@ -1,3 +1,24 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+
+class Token(models.Model):
+
+    unique_hash = models.CharField(
+        max_length=20,
+        validators=[
+            MinLengthValidator(20)
+        ],
+        verbose_name='Хэш токена'
+    )
+    tx_hash = models.CharField(
+        max_length=100,
+        verbose_name='Хэш транзакции создания токена'
+    )
+    media_url = models.URLField(
+        verbose_name='URL изображения'
+    )
+    owner = models.TextField(
+        max_length=100,
+        verbose_name='Адрес владельца в сети Ethereum'
+    )
